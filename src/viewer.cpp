2a1c42a3bbbf716753b8aeb80264d9b1b0e4e262
Brennan Shacklett <bpshacklett@gmail.com>
2023-07-05 20:44:26 -0700

    Fix color values

--- src/viewer.cpp
+++ src/viewer.cpp
@@ -7,13 +7,22 @@
 using namespace madrona;
 using namespace madrona::viz;
 
+static inline float srgbToLinear(float srgb)
+{
+    if (srgb <= 0.04045f) {
+        return srgb / 12.92f;
+    }
+
+    return powf((srgb + 0.055f) / 1.055f, 2.4f);
+}
+
 static inline math::Vector4 rgb8ToFloat(uint8_t r, uint8_t g, uint8_t b)
 {
     return {
-        (float)r / 255.f,
-        (float)g / 255.f,
-        (float)b / 255.f,
-        0.f,
+        srgbToLinear((float)r / 255.f),
+        srgbToLinear((float)g / 255.f),
+        srgbToLinear((float)b / 255.f),
+        1.f,
     };
 }
 
